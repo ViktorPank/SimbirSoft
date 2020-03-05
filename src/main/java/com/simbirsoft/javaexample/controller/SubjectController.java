ae6497069be4268fa0b637843dbd2165ece3c703
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-03-05 13:31:00 +0400

    Web-app

--- src/main/java/com/simbirsoft/javaexample/controller/SubjectController.java
+++ src/main/java/com/simbirsoft/javaexample/controller/SubjectController.java
@@ -1,11 +1,13 @@
 package com.simbirsoft.javaexample.controller;
 
 import com.simbirsoft.javaexample.dto.SubjectDto;
-import org.apache.coyote.Response;
+import com.simbirsoft.javaexample.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,43 +16,32 @@ import java.util.stream.Collectors;
 @RestController
 public class SubjectController {
 
-    private static List<SubjectDto> subjectDB = new ArrayList<>( );
+    @Autowired
+    private SubjectService subjectService;
 
-    static {
-        subjectDB.add(new SubjectDto("Java", 1));
-        subjectDB.add(new SubjectDto("Java", 1));
-        subjectDB.add(new SubjectDto("Шоттки", 2));
-        subjectDB.add(new SubjectDto("Физика", 6));
-    }
 
     @GetMapping("/subjects")
-    public List<SubjectDto> getSubjects(@RequestParam(name = "course") Integer course) {
-        return getFilterSubjects(course);
+    public ResponseEntity<List<SubjectDto>> getSubjects(HttpServletRequest request, @RequestParam(name = "course") Integer course) {
+        return ResponseEntity.ok(subjectService.getSubject(course));
 
     }
 
     @GetMapping("/subjects/{course}")
-    public List<SubjectDto> getSubject(@PathVariable(name = "course") Integer course) {
-        return getFilterSubjects(course);
+    public ResponseEntity<List<SubjectDto>> getSubject(@PathVariable(name = "course") Integer course) {
+        return ResponseEntity.ok(subjectService.getSubject(course));
 
     }
 
     @PostMapping("/subjects")
     public ResponseEntity addSubjects(@RequestBody SubjectDto subjectDto){
-
-        boolean isSuceed = subjectDB.add(subjectDto);
-
-        if (isSuceed) return ResponseEntity.ok().body("Запрос добавлен");
-        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Запрос не обработан");
-    }
-
-
-    private List<SubjectDto> getFilterSubjects(@RequestParam(name = "course") Integer course) {
-
-
-        return subjectDB.stream()
-                .filter(SubjectDto -> SubjectDto.getCourse() == course)
-                .collect(Collectors.toList());
+        boolean result = subjectService.addSubject(subjectDto);
+        if(!result){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
 
     }
+
+
+
 }
