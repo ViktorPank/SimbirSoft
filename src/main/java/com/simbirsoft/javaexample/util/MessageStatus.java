7d3ee57ac74294ae8af42cd4496c14dae46f47c4
Pavel Klimashin <pavel.klimashin@simbirsoft.com>
2020-03-25 14:10:23 +0400

    Замечания

--- src/main/java/com/simbirsoft/javaexample/util/MessageStatus.java
+++ src/main/java/com/simbirsoft/javaexample/util/MessageStatus.java
@@ -9,6 +9,9 @@ public class MessageStatus {
 
     private MessageSource messageStatus;
 
+    // TODO: Ты меня не совсем понял. Здесь и везде по коду где фигурируют локали не нужно получать LocaleContextHolder в параметре
+    // TODO: Просто в последнем месте где все сходится напиши LocaleContextHolder.getLocale()
+    // TODO: Почитай про то, как работают ThreadLocal поля - это даст тебе понимание как это (да и все остальные контексты) устроено
     public ResponseEntity getMessageErrorStatus(String message, LocaleContextHolder localeContext) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
